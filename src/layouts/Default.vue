5408c00bd4d8935ca3a3b79df8a9368b186a101e
Vyacheslav <vyacheslav.badaev@devit.group>
2021-10-13 07:17:31 +0000

    Scroll fix iOS

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -44,7 +44,7 @@ export default {
     handleScroll: function() {
       let mainNavLinks = document.querySelectorAll('.topic.active + ul .sub-topic')
       let fromTop = window.scrollY
-      if ((fromTop > 0 && this.pageScrolled == false) || (fromTop == 0 && this.pageScrolled == true)) {
+      if ((fromTop > 10 && this.pageScrolled == false) || (fromTop =< 10 && this.pageScrolled == true)) {
         this.pageScrolled = !this.pageScrolled
       }
       mainNavLinks.forEach(link => {
