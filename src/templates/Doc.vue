7767da588527e5140e79d34c3265ac1452f60f5c
Vyacheslav <vyacheslav.badaev@devit.group>
2021-11-03 16:25:43 +0000

    Added description meta

--- src/templates/Doc.vue
+++ src/templates/Doc.vue
@@ -22,7 +22,8 @@ query Doc ($path: String!) {
 export default {
   metaInfo() {
     return {
-      title: this.$page.doc.title
+      title: this.$page.doc.title,
+      description: this.$page.doc.description
     }
   }
 }
